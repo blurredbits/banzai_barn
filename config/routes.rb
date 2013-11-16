b02093fd265e72c3c57cd0835621065923e206b3
Blurredbits <mmorris00@mac.com>
2013-11-16 11:02:40 -0700

    Updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ BanzaiBarn::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#welcome'
+
+  get 'about' => 'static_pages#about'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
